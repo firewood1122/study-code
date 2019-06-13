a23d4062904c47ad2ddf0c146bd81c98b1caa1e8
lvjw <lvjw@xiaopeng.com>
2019-06-13 11:41:37 +0800

    feat: 初始化框架代码

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is a test page for study.
+      </header>
+    </div>
+  );
+}
+
+export default App;
