6f0ee95e0eb63acf8e8e24c164c9ce6130ade2de
firewood1122 <firewood_1122@163.com>
2021-10-22 07:09:31 +0000

    deploy: 67e91c96f13ad939bcfeffd78c67f8b530969cbb

---
+++ precache-manifest.cca7fbcba468bd0566036a633ec94bd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cde0abedcab6305e7d7b6504641524",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b5cb7016fa55cd091c",
+    "url": "/static/css/main.2d6fd300.chunk.css"
+  },
+  {
+    "revision": "9942f3611f72e1086ce3",
+    "url": "/static/js/2.1de42018.chunk.js"
+  },
+  {
+    "revision": "47b5cb7016fa55cd091c",
+    "url": "/static/js/main.7a427f98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
